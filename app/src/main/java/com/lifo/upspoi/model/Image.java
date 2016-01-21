4c518460e5c888731b03cb1e58d09d180f19b15e
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-01-21 17:14:41 +0100

    #7 Prise en compte que chaque image doit être associée au pseudo de l'utilisateur

--- app/src/main/java/com/lifo/upspoi/model/Image.java
+++ app/src/main/java/com/lifo/upspoi/model/Image.java
@@ -7,8 +7,11 @@ public class Image {
 
     private String imageURL;
 
-    public Image(String imageURL) {
+    private Utilisateur createur;
+
+    public Image(String imageURL, Utilisateur createur) {
         this.imageURL = imageURL;
+        this.createur = createur;
     }
 
     public String getImageURL() {
@@ -17,5 +20,13 @@ public class Image {
 
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
+    }
+
+    public Utilisateur getCreateur() {
+        return createur;
+    }
+
+    public void setCreateur(Utilisateur createur) {
+        this.createur = createur;
     }
 }
