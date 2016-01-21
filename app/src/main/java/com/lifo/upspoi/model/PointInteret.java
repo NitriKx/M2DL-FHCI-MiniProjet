3e9c48038fd219167536848281815bbed0df7b0d
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-01-21 14:17:25 +0100

    #3 Ajout de classes de modèle de base

---
+++ app/src/main/java/com/lifo/upspoi/model/PointInteret.java
@@ -0,0 +1,7 @@
+package com.lifo.upspoi.model;
+
+/**
+ * Created by benoit on 21/01/16.
+ */
+public class PointInteret {
+}
---
+++ app/src/main/java/com/lifo/upspoi/services/PointInteretService.java
@@ -0,0 +1,7 @@
+package com.lifo.upspoi.services;
+
+/**
+ * Created by benoit on 21/01/16.
+ */
+public class PointInteretService {
+}
