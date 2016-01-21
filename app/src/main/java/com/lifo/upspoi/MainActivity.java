1b1dd00e45a64ad2de46389b68ed5d572940cc9b
lpierillas <lucie.pierillas@gmail.com>
2016-01-21 14:48:16 +0100

    #5 zoom sur Paul Sabatier a l'ouverture de l'application

--- app/src/main/java/com/lifo/upspoi/MainActivity.java
+++ app/src/main/java/com/lifo/upspoi/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.lifo.upspoi.model.Utilisateur;
@@ -48,11 +49,15 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng universite = new LatLng(43.560724, 1.468703);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(universite)      // Centre la map sur Paul Sabatier
+                .zoom(15)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 
 
