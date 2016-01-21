2703b41c0d3f22b3ffb4fa71c7e79f6703d8efe2
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-01-21 14:32:50 +0100

    #4 Création des méthodes de service désignée + singleton des services

--- app/src/main/java/com/lifo/upspoi/services/PointInteretService.java
+++ app/src/main/java/com/lifo/upspoi/services/PointInteretService.java
@@ -1,7 +1,30 @@
 package com.lifo.upspoi.services;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.lifo.upspoi.model.PointInteret;
+
+import java.util.List;
+
 /**
  * Created by Benoît Sauvère on 21/01/16.
  */
 public class PointInteretService {
+
+    public List<PointInteret> getPointInteretDansZone(LatLng topleft, LatLng bottomright) {
+        return null;
+    }
+
+
+    //
+    //  SINGLETON
+    //
+
+    private static final PointInteretService __instance = new PointInteretService();
+
+    private PointInteretService() {}
+
+    public static PointInteretService getInstance() {
+        return __instance;
+    }
+
 }
--- app/src/main/java/com/lifo/upspoi/services/UtilisateurService.java
+++ app/src/main/java/com/lifo/upspoi/services/UtilisateurService.java
@@ -1,7 +1,31 @@
 package com.lifo.upspoi.services;
 
+import com.lifo.upspoi.model.Utilisateur;
+
 /**
  * Created by Benoît Sauvère on 21/01/16.
  */
 public class UtilisateurService {
+
+
+    public Utilisateur getUtilisateurPourPseudo(String pseudo) {
+        return null;
+    }
+
+
+
+
+
+    //
+    //  SINGLETON
+    //
+
+    private static final UtilisateurService __instance = new UtilisateurService();
+
+    private UtilisateurService() {}
+
+    public static UtilisateurService getInstance() {
+        return __instance;
+    }
+
 }
